920dfd5f9a83362fca28f86aeb3877a51fffa047
Jerome <defsv8@gmail.com>
2020-11-28 11:22:52 -0500

    Delete Low Poly + clean up code

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -6,7 +6,6 @@ public class CharacterMovement : MonoBehaviour
 
     private Vector2 _direction;
 
-    private Rigidbody _rigidbody;
     [SerializeField] private float _speed = 4;
     [SerializeField] private float _turnSpeed = 2f;
     CharacterController _characterController;
@@ -14,7 +13,6 @@ public class CharacterMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //_rigidbody = GetComponent<Rigidbody>();
         _characterController = GetComponent<CharacterController>();
     }
 
@@ -29,23 +27,21 @@ public class CharacterMovement : MonoBehaviour
         Vector3 _forward = Camera.main.transform.forward;
         _forward.y = 0;
         var move = _direction.y * _forward + _direction.x * Camera.main.transform.right;
-       // move.y = 0;
 
         if (move != Vector3.zero)
         {
-            //transform.forward = Vector3.Lerp(transform.forward, move.normalized, _turnSpeed);
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(move), Time.deltaTime * _turnSpeed);
             var speed = _speed;
 
             //Apply gravity 
             move.y += Physics.gravity.y * Time.fixedDeltaTime * 5f;
-            //transform.position = transform.position + move * speed * Time.fixedDeltaTime;
+
             _characterController.Move(move * speed * Time.fixedDeltaTime);
 
         }
         else
         {
-            //_rigidbody.angularVelocity = Vector3.zero;
+            //Reset Velocity?
             
         }
    
