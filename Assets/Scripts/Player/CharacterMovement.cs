e55097b7e3f9caba7bb63e76451f3210019cb669
Jerome <defsv8@gmail.com>
2020-11-28 17:50:47 -0500

    Billy Animation

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -9,11 +9,13 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private float _speed = 4;
     [SerializeField] private float _turnSpeed = 5f;
     CharacterController _characterController;
+    Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -34,13 +36,19 @@ public class CharacterMovement : MonoBehaviour
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(move), Time.fixedDeltaTime * _turnSpeed * move.magnitude);
             var speed = _speed;
-
+            _animator.SetFloat("Speed", 0.2f);
+            float playBackSpeed = 0.25f *1;
+            Debug.Log("Playback Speed:" + _direction.magnitude);// move.magnitude / (speed * Time.fixedDeltaTime));
+            _animator.speed = _direction.magnitude * 2f;
+            Mathf.Clamp(playBackSpeed, 0.5f, 1.5f);
             _characterController.Move(move * speed * Time.fixedDeltaTime);
 
         }
         else
         {
             //Reset Velocity?
+            _animator.SetFloat("Speed", 0);
+            _animator.speed = 1;
             
         }
    
