2475dafb5a2762c6c3984ccf6c31704b18d0d96a
Jerome <defsv8@gmail.com>
2020-11-28 01:53:32 -0500

    Interaction System, Interactable Prefabs, Auto PickUp Prefabs, Player Prefabs

---
+++ Assets/Scripts/Interaction System/AutoPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoPickup : MonoBehaviour, IInteractble
+{
+    public float interactionRange => 2f;
+
+    public bool isAutoPickup => true;
+
+    public void OnExitRange()
+    {
+        return;
+    }
+
+    public void OnEnterRange()
+    {
+        return;
+    }
+
+    public void OnInteract()
+    {
+        Debug.Log("Auto Pickup");
+    }
+}
---
+++ Assets/Scripts/Interaction System/BasicInteraction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BasicInteraction : MonoBehaviour,IInteractble
+{
+    public float interactionRange => 2f;
+
+    public bool isAutoPickup => false;
+
+    public void OnEnterRange()
+    {
+        Debug.Log("Entering Interaction Range");
+    }
+
+    public void OnExitRange()
+    {
+        Debug.Log("Exiting Interaction Range");
+    }
+
+    public void OnInteract()
+    {
+        Debug.Log("Interacting");
+    }
+}
