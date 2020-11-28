4b8fd41f72f62f49c795de2b490d1ee0983a06af
Maxime Charland <charland.maxime@gmail.com>
2020-11-28 00:09:02 -0500

    Create Level transitions framework with sample

---
+++ Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitions
+{
+    public delegate void LevelEndedHandler(object sender, bool? victory); // True = Victory, False = Defeat, null = Neutral
+    public delegate void LevelSetupFinishedHandler(object sender);
+    public delegate void LevelTransitionFinishedHandler(object sender);
+}
