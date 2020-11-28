fe7606f84856f31bb152c7fd4e8b62983f3c427f
Hoshikko9 <roxannecorbeil@hotmail.com>
2020-11-28 08:40:04 -0500

    Basic UI

---
+++ Assets/Scripts/UI/KeyView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KeyView : MonoBehaviour
+{
+    public TextMeshProUGUI KeysAmountText;
+
+    private void Start()
+    {
+        // TODO: Get in inventory
+        int amount = 2;
+        KeysAmountText.text = $"x{amount}";
+    }
+
+    //Will be activated with inventory
+    private void OnKeyAmountChanged()
+    {
+        int keyAmount = 2;
+        KeysAmountText.text = $"x{keyAmount}";
+    }
+}
