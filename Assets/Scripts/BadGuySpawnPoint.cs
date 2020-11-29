97727154e9da2d2762c9d54f91b9194c08044ce5
Maxime Charland <charland.maxime@gmail.com>
2020-11-28 20:29:26 -0500

    Bad Guy Spawn

---
+++ Assets/Scripts/BadGuySpawnPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BadGuySpawnPoint : MonoBehaviour, IInteractble
+{
+    public GameObject BadGuy;
+
+    public float interactionRange => 2f;
+
+    public bool isAutoPickup => false;
+
+    public void OnEnterRange() {}
+
+    public void OnExitRange() {}
+
+    public void OnInteract()
+    {
+        var awaiter = Task.Delay(1000).GetAwaiter();
+        awaiter.OnCompleted(() =>
+        {
+            gameObject.SetActive(false);
+            BadGuy.SetActive(true);
+        });
+    }
+}
