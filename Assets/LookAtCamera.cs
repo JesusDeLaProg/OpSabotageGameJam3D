f255f9bf034602f39b2d151ca64ae460a9060603
Jerome <defsv8@gmail.com>
2020-11-29 00:37:21 -0500

    Rotation

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    Transform _cameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 lookPos = _cameraTransform.position - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.fixedDeltaTime * 2f);
+    }
+}
